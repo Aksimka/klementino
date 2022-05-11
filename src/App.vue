6605e2d93aadfd8efa6779245ac2e799a3beda60
Aksimka <mks_comp97@mail.ru>
2022-05-11 14:38:22 +0300

    hidden fix

--- src/App.vue
+++ src/App.vue
@@ -40,5 +40,6 @@ export default defineComponent({
   height: calc(100vh - var(--nav-heigth) - var(--header-heigth));
   margin-top: var(--header-heigth);
   touch-action: pan-y;
+  overflow-x: hidden;
 }
 </style>
